d9b9bc81e12d986df5a097d3d36a70a64691178c
lesedi19 <97071316+lesedi19@users.noreply.github.com>
2024-01-08 10:22:23 +0700

    voting photo

--- src/components/voting.jsx
+++ src/components/voting.jsx
@@ -106,7 +106,7 @@ const VotingComponent = () => {
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
                 {/* <img width="100%" src={img_01} width={"100%"} alt=" /> */}
-                <img width="100%" src="https://scontent.fhan3-5.fna.fbcdn.net/v/t39.30808-6/407098048_751026813733740_5588014595165944053_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeELgbhZIaX5dE-FXX0VhA1WnrnuuenXnGeeue656decZ2IKLy6KxDfEgoicb_IHsKrdKH5v2LAidfgkN8FafVoY&_nc_ohc=04BVmfxh358AX9tnV1L&_nc_ht=scontent.fhan3-5.fna&oh=00_AfB-oqG3ThtF0CN-6GcYD5sozy55L0EQu2UyryCCBiKx_w&oe=659EDE83" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703818994432.jpg&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD:3058 | FPT</b></p>
@@ -121,7 +121,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703751034461.JPG&w=1920&q=75" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703755921770.jpg&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD:1124 | FPT</b></p>
@@ -136,7 +136,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/408514724_751026763733745_704825905293092306_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeFojtNnJmWds6h6GEsXkz-UTrXisuuAWc9OteKy64BZz9ZM53Sx0w-J_UmrgR1rp2BDxumeqqjECX7QXKlIJJM5&_nc_ohc=ped-A2ZmuJcAX88L03B&_nc_ht=scontent.fhan3-4.fna&oh=00_AfAbvAUPDH5ggIxecAqfR-p3se57ZPTORG82lC-dakl6Cw&oe=659E9708" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703750984671.JPG&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD:3001 | FPT</b></p>
@@ -151,7 +151,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703753270738.JPEG&w=1920&q=75" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703752300363.jpg&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD:2032 | VLU</b></p>
@@ -166,7 +166,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703755582821.JPG&w=1920&q=75" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703752658148.JPG&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD: 2063 | UTE</b></p>
@@ -181,7 +181,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://scontent.fhan3-5.fna.fbcdn.net/v/t39.30808-6/405414623_751026800400408_1391227485058402166_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeE0yosvbXNQ2ian1pTVZYZTJAiczPu3JbMkCJzM-7clswi74MsJGWyz4oUbRlcThhQm3TEag2TSECKz2Ju84F0v&_nc_ohc=GiibUVWFnZEAX9NBAoE&_nc_ht=scontent.fhan3-5.fna&oh=00_AfCSilvwWOexauYBxDRujiI-y-pJm19x9AQutZbjY9Oy4w&oe=659DC3E0" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703750369110.jpg&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD:1127 | VLU</b></p>
@@ -196,7 +196,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703750369110.jpg&w=1920&q=75" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703818920914.jpg&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD: 1861 | HANU </b></p>
@@ -211,7 +211,7 @@ const VotingComponent = () => {
 
             <div className="detail-user"  onClick={showAlert}>
               <div className="thumnail">
-                <img width="100%" src="https://scontent.fhan3-5.fna.fbcdn.net/v/t39.30808-6/405478324_751026773733744_5306740271661205502_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEDp2pYBZQX0S1PqBb4c-5mez02P7Gh7Tt7PTY_saHtO-m4ls_BK-sS5tKdqf3nJe49HULetOQHkDn6G1mC4F3W&_nc_ohc=T9lbmAmsWLwAX-0_MPS&_nc_ht=scontent.fhan3-5.fna&oh=00_AfCdRfNQHVVBiNzh_cA40cL-6Ymjon3ci8fhLNMvfLpbgQ&oe=659EC5C8" />
+                <img width="100%" src="https://hoakhoisinhvienvietnam.1vote.vn/_next/image?url=https%3A%2F%2Fmedia-platform.1vote.vn%2Fthumbnails%2Fuploads%2FxUrn1%2F1703752194026.JPG&w=1920&q=75" />
               </div>
               <div className="content">
                 <p><b>SBD:2015 | FPT</b></p>
